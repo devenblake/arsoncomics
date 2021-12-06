be6ec7070074b7b4c6627357c23fd0fc0f8d8512
Deven Blake <trinity@trinity.moe>
2021-12-06 12:24:24 -0500

    refine JS

--- js/hop.js
+++ js/hop.js
@@ -1,20 +1,32 @@
 /* hop.js; Deven Blake 2021; Public domain */
 
-window.hop_button_stuff = function(){
+window.hop_DIGITS = 4;
+
+window.hop_button_action = function(){
 	var hop_box_value;
+
+	if((hop_box_value = hop_get_intended_value()) != -1)
+		window.hop_to_position(hop_box_value);
+};
+
+window.hop_get_intended_value = function(){
+	var hop_box_value;
+
 	if(isNaN(hop_box_value = parseInt(document.getElementById("hop_box").value)))
 		/* not valid value */
 		document.getElementById("hop_feedback").innerHTML =
 			"'" + document.getElementById("hop_box").value + "'"
 			+ " is not a valid number."
 		;
-	else if(hop_box_value > 9999 || hop_box_value < 1)
-		/* too high */
+	else if(hop_box_value >= Math.pow(10, window.hop_DIGITS) || hop_box_value < 1)
+		/* out of bounds */
 		document.getElementById("hop_feedback").innerHTML =
-			"There are no arson comics succeeding 9999 or preceding 1."
-		;
+			"There are no arson comics succeeding "
+			+ (Math.pow(10, window.hop_DIGITS) - 1).toString()
+			+ " or preceding 1."
 	else
-		window.hop_to_position(hop_box_value);
+		return hop_box_value;
+	return -1;
 };
 
 window.hop_to_position = function(position){
@@ -25,21 +37,23 @@ window.hop_to_position = function(position){
 	var post_hash;
 
 	current_href = window.location.href;
-	post_hash = (position % 10).toString();
 
 	pre_hash = ((position - (position % 10)) / 10).toString();
-	while(pre_hash.length < 3)
+	while(pre_hash.length < window.hop_DIGITS - 1)
 		pre_hash = "0" + pre_hash;
 
 	/* If the URL ends with '/', remove the trailing '/' */
 	if(current_href.charAt(current_href.length - 1) == '/')
-		/* This was my original method: */
-		/* current_href = Array.from(current_href).reverse().slice(1).reverse().join(''); */
-		/* This is much easier: */
 		current_href = current_href.substring(0, current_href.length - 1);
 
-	hop_destination = window.location.href.split('/');
+/* 'website/000' */
+	hop_destination = current_href.split('/');
+/* 'website', '000' */
 	hop_destination.pop();
-	hop_destination = hop_destination.join('/') + "/" + pre_hash + "#" + post_hash;
-	window.location.href = hop_destination;
+/* 'website' */
+	window.location.href = hop_destination.join('/')
+/* 'website' */
+		+ "/" + pre_hash + "#" + (position % 10).toString()
+/* 'website/000#1' */
+	;
 };
